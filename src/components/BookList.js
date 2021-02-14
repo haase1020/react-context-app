6f1d72fa7a5c5b174d2b4fe4fd21d61ea6340f8b
Mandi <haase1020@gmail.com>
2021-02-14 03:26:22 -0500

    initial commit

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class BookList extends Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>Narnia</li>
+          <li>Frindle</li>
+          <li>Hope was Here</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
